309219c9c6da30c898ba5dd77d1b2c9ebc5898c7
bpakra200 <bidit_pakrashi@comcast.com>
2022-04-17 21:02:04 -0400

    Add svelte

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	let name = 'Bidit';
+	let number = 1;
+
+	function incrementNumber() {
+		number += 1;
+	}
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {name}, my Number is {number}!</h1>
+<button on:click="{incrementNumber}">Change Number</button>
